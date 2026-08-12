06220e4a2dc533b1cab517477c970fb64207d54f
Vyankatesh Rohokale <piyushrohokale2525@gmail.com>
2026-08-12 13:01:41 +0530

    a1

---
+++ from_bin/BankAccount.java
@@ -0,0 +1,36 @@
+class BankAccount {
+    int accountNumber;
+    String accountHolder;
+    double balance;
+
+    // Default Constructor
+    BankAccount() {
+        accountNumber = 1001;
+        accountHolder = "Rahul";
+        balance = 5000.0;
+    }
+
+    // Method
+    void displayAccountDetails() {
+        System.out.println("Account Number : " + accountNumber);
+        System.out.println("Account Holder : " + accountHolder);
+        System.out.println("Balance : ₹" + balance);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Java Environment Setup Successful!");
+
+        String bankName = "State Bank of India";
+        int branchCode = 101;
+        double interestRate = 6.5;
+
+        System.out.println("Bank Name : " + bankName);
+        System.out.println("Branch Code : " + branchCode);
+        System.out.println("Interest Rate : " + interestRate + "%");
+
+        BankAccount account = new BankAccount();
+
+        account.displayAccountDetails();
+    }
+}
